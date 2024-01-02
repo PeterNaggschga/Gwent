9968e2359d07a3467501a505ab45d9df5d35ccaa
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-01-02 21:51:45 +0100

    added documentation to UnitEntity
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/gwent/data/UnitEntity.java
+++ app/src/main/java/com/peternaggschga/gwent/data/UnitEntity.java
@@ -1,5 +1,8 @@
 package com.peternaggschga.gwent.data;
 
+import static org.valid4j.Assertive.require;
+
+import androidx.annotation.IntRange;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.room.ColumnInfo;
@@ -7,91 +10,207 @@ import androidx.room.Entity;
 import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
+/**
+ * A class representing a card on the game board.
+ * Is a persistent Entity and is therefore saved in a database table named `units`.
+ */
 @Entity(tableName = "units", foreignKeys = {
         @ForeignKey(entity = RowEntity.class,
                 parentColumns = "id",
                 childColumns = "row",
                 onDelete = ForeignKey.CASCADE)})
+@SuppressWarnings("unused")
 public class UnitEntity {
+    /**
+     * Defines the primary key of the represented unit.
+     * Is generated automatically on insert.
+     */
     @PrimaryKey(autoGenerate = true)
     private int id;
 
+    /**
+     * Defines whether the represented card is epic.
+     * Is set to `false` by default.
+     */
     @ColumnInfo(defaultValue = "false")
     private boolean epic;
 
+    /**
+     * Defines the base-damage of the represented card.
+     * Must be non-negative.
+     */
     private int damage;
 
+    /**
+     * Defines the Ability of the represented card.
+     * Is set to Ability#NONE by default.
+     * Must not be `null`.
+     */
     @ColumnInfo(defaultValue = "NONE")
     @NonNull
     private Ability ability;
 
+    /**
+     * Defines the squad the unit belongs to if #ability is Ability#BINDING.
+     * If #ability is anything else, this value is `null`.
+     */
+    @ColumnInfo(defaultValue = "NULL")
+    @IntRange(from = 0)
     @Nullable
     private Integer squad;
 
+    /**
+     * Defines the attack row the card lies in.
+     * Must not be `null`.
+     */
     @ColumnInfo(index = true)
     @NonNull
     private RowType row;
 
-    public UnitEntity(boolean epic, int damage, @NonNull Ability ability, @Nullable Integer squad, @NonNull RowType row) {
+    /**
+     * Constructor of a UnitEntity.
+     * #id may not be set here since the value is generated automatically.
+     *
+     * @param epic    Boolean representing whether card is #epic.
+     * @param damage  Non-negative value representing the #damage of the card.
+     * @param ability Ability representing the #ability of the card.
+     * @param squad   Integer representing the #squad of a card that has the Ability#BINDING #ability.
+     * @param row     RowType representing the combat type of the card.
+     */
+    UnitEntity(boolean epic, @IntRange(from = 0) int damage, @NonNull Ability ability, @IntRange(from = 0) @Nullable Integer squad, @NonNull RowType row) {
+        require(damage >= 0);
+        require(ability == Ability.BINDING || squad == null);
+        require(ability != Ability.BINDING || (squad != null && squad >= 0));
         this.epic = epic;
-        assert damage >= 0;
         this.damage = damage;
         this.ability = ability;
-        assert squad == null || (ability == Ability.BINDING && squad >= 0);
         this.squad = squad;
         this.row = row;
     }
 
-    public int getId() {
+    /**
+     * Getter for #id.
+     * Only used by Room extension.
+     *
+     * @return Integer representing the units' id.
+     */
+    int getId() {
         return id;
     }
 
+    /**
+     * Setter for #id.
+     * Only used by Room extension.
+     *
+     * @param id Integer representing the units' id.
+     */
     void setId(int id) {
         this.id = id;
     }
 
-    public boolean isEpic() {
+    /**
+     * Getter for #epic.
+     * Only used by Room extension.
+     *
+     * @return Boolean representing whether the card is epic.
+     */
+    boolean isEpic() {
         return epic;
     }
 
-    public void setEpic(boolean epic) {
+    /**
+     * Setter for #epic.
+     * Only used by Room extension.
+     *
+     * @param epic Boolean representing whether the card is epic.
+     */
+    void setEpic(boolean epic) {
         this.epic = epic;
     }
 
-    public int getDamage() {
+    /**
+     * Getter for #damage.
+     * Only used by Room extension.
+     *
+     * @return Integer representing the card's base-damage.
+     */
+    int getDamage() {
         return damage;
     }
 
-    public void setDamage(int damage) {
-        assert damage >= 0;
+    /**
+     * Setter for #damage.
+     * Only used by Room extension.
+     *
+     * @param damage Integer representing the card's base-damage.
+     */
+    void setDamage(@IntRange(from = 0) int damage) {
+        require(damage >= 0);
         this.damage = damage;
     }
 
+    /**
+     * Getter for #ability.
+     * Only used by Room extension.
+     *
+     * @return Ability representing the units' ability.
+     */
     @NonNull
-    public Ability getAbility() {
+    Ability getAbility() {
         return ability;
     }
 
-    public void setAbility(@NonNull Ability ability) {
+    /**
+     * Setter for #ability.
+     * Only used by Room extension.
+     *
+     * @param ability Ability representing the units' ability.
+     */
+    void setAbility(@NonNull Ability ability) {
         this.ability = ability;
     }
 
+    /**
+     * Getter for #squad.
+     * Only used by Room extension.
+     *
+     * @return Integer representing the units' squad if #ability is Ability#BINDING or `null`.
+     */
     @Nullable
-    public Integer getSquad() {
+    Integer getSquad() {
         return squad;
     }
 
-    public void setSquad(@Nullable Integer squad) {
-        assert squad == null || (ability == Ability.BINDING && squad >= 0);
+    /**
+     * Setter for #squad.
+     * Only used by Room extension.
+     *
+     * @param squad Integer representing the units' squad if #ability is Ability#BINDING or `null`.
+     */
+    void setSquad(@IntRange(from = 0) @Nullable Integer squad) {
+        require(ability == Ability.BINDING || squad == null);
+        require(ability != Ability.BINDING || (squad != null && squad >= 0));
         this.squad = squad;
     }
 
+    /**
+     * Getter for #row.
+     * Only used by Room extension.
+     *
+     * @return RowType representing the units combat row.
+     */
     @NonNull
-    public RowType getRow() {
+    RowType getRow() {
         return row;
     }
 
-    public void setRow(@NonNull RowType row) {
+    /**
+     * Setter for #row.
+     * Only used by Room extension.
+     *
+     * @param row RowType representing the units combat row.
+     */
+    void setRow(@NonNull RowType row) {
         this.row = row;
     }
 }
