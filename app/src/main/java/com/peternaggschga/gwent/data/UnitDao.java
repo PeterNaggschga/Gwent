4db6bc9a8df1bee6e6312a9fef9b0d425ab38102
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-01-02 13:12:18 +0100

    updated UnitDao to incorporate RxJava types
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/gwent/data/UnitDao.java
+++ app/src/main/java/com/peternaggschga/gwent/data/UnitDao.java
@@ -8,20 +8,23 @@ import androidx.room.Query;
 
 import java.util.List;
 
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Single;
+
 @Dao
 interface UnitDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    void insertUnit(UnitEntity unit);
+    Completable insertUnit(UnitEntity unit);
 
     @Delete
-    void deleteUnit(UnitEntity unit);
+    Completable deleteUnit(UnitEntity unit);
 
     @Query("SELECT * FROM units WHERE id = :id")
-    UnitEntity getUnit(int id);
+    Single<UnitEntity> getUnit(int id);
 
     @Query("SELECT * FROM units WHERE `row` = :row")
-    List<UnitEntity> getUnits(RowType row);
+    Single<List<UnitEntity>> getUnits(RowType row);
 
     @Query("SELECT * FROM units")
-    List<UnitEntity> getUnits();
+    Single<List<UnitEntity>> getUnits();
 }
