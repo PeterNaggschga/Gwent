34b791c7436d84eeafd7f78920f52787853ef207
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-01-02 15:52:12 +0100

    removed indices from RowType
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/gwent/data/RowType.java
+++ app/src/main/java/com/peternaggschga/gwent/data/RowType.java
@@ -1,17 +1,7 @@
 package com.peternaggschga.gwent.data;
 
 public enum RowType {
-    MELEE(0),
-    RANGE(1),
-    SIEGE(2);
-
-    private final Integer index;
-
-    RowType(final Integer index) {
-        this.index = index;
-    }
-
-    public final Integer getIndex() {
-        return index;
-    }
+    MELEE,
+    RANGE,
+    SIEGE
 }
