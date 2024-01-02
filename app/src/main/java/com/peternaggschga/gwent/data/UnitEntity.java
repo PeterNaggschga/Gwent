f8ae133796a2187c6056b03537cab9b7652f608b
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-01-02 13:06:41 +0100

    made fields of UnitEntity private
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/gwent/data/UnitEntity.java
+++ app/src/main/java/com/peternaggschga/gwent/data/UnitEntity.java
@@ -13,25 +13,69 @@ import androidx.room.PrimaryKey;
                 onDelete = ForeignKey.CASCADE)})
 public class UnitEntity {
     @PrimaryKey(autoGenerate = true)
-    public int id;
+    private int id;
 
     @ColumnInfo(defaultValue = "false")
-    public boolean epic;
+    private boolean epic;
 
-    public int damage;
+    private int damage;
 
     @ColumnInfo(defaultValue = "NONE")
     @NonNull
-    public Ability ability;
+    private Ability ability;
 
     @ColumnInfo(index = true)
     @NonNull
-    public RowType row;
+    private RowType row;
 
     public UnitEntity(boolean epic, int damage, @NonNull Ability ability, @NonNull RowType row) {
         this.epic = epic;
+        assert damage >= 0;
         this.damage = damage;
         this.ability = ability;
+        this.row = row;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isEpic() {
+        return epic;
+    }
+
+    public void setEpic(boolean epic) {
+        this.epic = epic;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        assert damage >= 0;
+        this.damage = damage;
+    }
+
+    @NonNull
+    public Ability getAbility() {
+        return ability;
+    }
+
+    public void setAbility(@NonNull Ability ability) {
+        this.ability = ability;
+    }
+
+    @NonNull
+    public RowType getRow() {
+        return row;
+    }
+
+    public void setRow(@NonNull RowType row) {
         this.row = row;
     }
 }
