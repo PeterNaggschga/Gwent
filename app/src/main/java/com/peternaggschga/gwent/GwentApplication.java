c57304ca342ab2922dd4a5578e7aa5c6be687fc8
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-01-19 16:51:37 +0100

    added GwentApplication.java
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

---
+++ app/src/main/java/com/peternaggschga/gwent/GwentApplication.java
@@ -0,0 +1,22 @@
+package com.peternaggschga.gwent;
+
+import android.app.Application;
+
+import androidx.room.Room;
+
+import com.peternaggschga.gwent.data.AppDatabase;
+import com.peternaggschga.gwent.data.UnitRepository;
+
+public class GwentApplication extends Application {
+    private AppDatabase database;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        database = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database").build();
+    }
+
+    public UnitRepository getRepository() {
+        return new UnitRepository(database);
+    }
+}
