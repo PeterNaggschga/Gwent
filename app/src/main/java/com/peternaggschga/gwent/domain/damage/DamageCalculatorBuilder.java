85db329ce6c18971252e991fe44d9ad010ff6493
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-01-21 01:01:52 +0100

    added DamageCalculatorBuilder.java
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

---
+++ app/src/main/java/com/peternaggschga/gwent/domain/damage/DamageCalculatorBuilder.java
@@ -0,0 +1,30 @@
+package com.peternaggschga.gwent.domain.damage;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+import java.util.Map;
+
+class DamageCalculatorBuilder {
+    private DamageCalculator calculator;
+
+    void setWeather(boolean weather) {
+        calculator = new WeatherDamageCalculator(weather);
+    }
+
+    void setBond(@NonNull Map<Integer, Integer> idToSquad) {
+        calculator = new BondDamageCalculatorDecorator(calculator, idToSquad);
+    }
+
+    void setMoral(@NonNull List<Integer> unitIds) {
+        calculator = new MoralDamageCalculatorDecorator(calculator, unitIds);
+    }
+
+    void setHorn(@NonNull List<Integer> unitIds) {
+        calculator = new HornDamageCalculatorDecorator(calculator, unitIds);
+    }
+
+    public DamageCalculator getResult() {
+        return calculator;
+    }
+}
