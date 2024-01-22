f611b9f8623229b7ac5706305b9948284bdbe552
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-01-22 11:12:38 +0100

    added documentation for DamageCalculatorDecorator
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/gwent/domain/damage/DamageCalculatorDecorator.java
+++ app/src/main/java/com/peternaggschga/gwent/domain/damage/DamageCalculatorDecorator.java
@@ -1,9 +1,23 @@
 package com.peternaggschga.gwent.domain.damage;
 
+import androidx.annotation.NonNull;
+
+/**
+ * An abstract DamageCalculator
+ * that is used as a superclass for the decorator classes implementing the damage calculation.
+ */
 abstract class DamageCalculatorDecorator implements DamageCalculator {
+    /**
+     * A DamageCalculator which is decorated by this decorator.
+     */
     protected DamageCalculator component;
 
-    DamageCalculatorDecorator(DamageCalculator component) {
+    /**
+     * Constructor of a DamageCalculatorDecorator.
+     *
+     * @param component DamageCalculator that is being decorated by this decorator.
+     */
+    DamageCalculatorDecorator(@NonNull DamageCalculator component) {
         this.component = component;
     }
 }
