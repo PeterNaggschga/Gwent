792d80e48b35c4d7574f22cd317c708dd02765cd
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-01-26 00:18:48 +0100

    added documentation to BondDamageCalculatorDecorator
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/gwent/domain/damage/BondDamageCalculatorDecorator.java
+++ app/src/main/java/com/peternaggschga/gwent/domain/damage/BondDamageCalculatorDecorator.java
@@ -1,19 +1,51 @@
 package com.peternaggschga.gwent.domain.damage;
 
+import static org.valid4j.Assertive.require;
+
+import androidx.annotation.IntRange;
+import androidx.annotation.NonNull;
+
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * A DamageCalculator class responsible for calculating the tight bond buff if necessary,
+ * i.e., if there are units with the com.peternaggschga.gwent.data.Ability#BINDING ability.
+ * Acts as a ConcreteDecorator in the implemented decorator pattern and must decorate a WeatherDamageCalculator.
+ */
 class BondDamageCalculatorDecorator extends DamageCalculatorDecorator {
-    private final Map<Integer, Integer> idToSquad;
+    /**
+     * A Map
+     * mapping the ids of all units with the com.peternaggschga.gwent.data.Ability#BINDING ability to the respective squad size.
+     */
+    private final Map<Integer, Integer> idToSquadSize;
 
-    BondDamageCalculatorDecorator(DamageCalculator component, Map<Integer, Integer> idToSquadSize) {
+    /**
+     * Constructor of a BondDamageCalculatorDecorator.
+     * Should only be called by DamageCalculatorBuilder.
+     *
+     * @param component     WeatherDamageCalculator that is being decorated by this decorator.
+     * @param idToSquadSize Map mapping the ids of all units with the com.peternaggschga.gwent.data.Ability#BINDING ability to the respective squad size.
+     * @see DamageCalculatorBuilder
+     */
+    BondDamageCalculatorDecorator(@NonNull WeatherDamageCalculator component, @NonNull Map<Integer, Integer> idToSquadSize) {
         super(component);
-        this.idToSquad = idToSquadSize;
+        this.idToSquadSize = idToSquadSize;
     }
 
+    /**
+     * Calculates the (de-)buffed damage of unit with the given id and the given base-damage.
+     * Returns the given damage times the number of units with the same squad
+     * if the unit has the com.peternaggschga.gwent.data.Ability#BINDING ability.
+     *
+     * @param id     Integer representing the UnitEntity#id of the unit whose (de-)buff damage is calculated.
+     * @param damage Integer representing the base-damage of the unit whose (de-)buff damage is calculated.
+     * @return Integer representing the (de-)buffed damage of the unit.
+     */
     @Override
-    public int calculateDamage(int id, int damage) {
+    public int calculateDamage(int id, @IntRange(from = 0) int damage) {
+        require(damage >= 0);
         int componentDamage = component.calculateDamage(id, damage);
-        return idToSquad.containsKey(id) ? componentDamage * Objects.requireNonNull(idToSquad.get(id)) : componentDamage;
+        return idToSquadSize.containsKey(id) ? componentDamage * Objects.requireNonNull(idToSquadSize.get(id)) : componentDamage;
     }
 }
