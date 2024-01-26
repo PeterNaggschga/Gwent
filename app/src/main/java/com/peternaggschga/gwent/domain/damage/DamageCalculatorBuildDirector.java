dfde7133dd8bba58d08e2a7dce07b97106bc8661
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-01-26 18:29:14 +0100

    added documentation to DamageCalculatorBuildDirector
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/gwent/domain/damage/DamageCalculatorBuildDirector.java
+++ app/src/main/java/com/peternaggschga/gwent/domain/damage/DamageCalculatorBuildDirector.java
@@ -16,7 +16,22 @@ import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+/**
+ * A build director responsible for the creation of DamageCalculator instances from sets of units using DamageCalculatorBuilder.
+ * Encapsulates the logic for correct order of decorators.
+ *
+ * @see DamageCalculatorBuilder
+ */
 public class DamageCalculatorBuildDirector {
+    /**
+     * Creates a DamageCalculator for a row with the given weather status, horn status and units.
+     *
+     * @param weather Boolean defining whether the calculation encompasses the weather debuff.
+     * @param horn    Boolean defining whether a commander's horn is in the row.
+     * @param units   Collection of UnitEntity objects that are in the row.
+     * @return A DamageCalculator object capable of calculating damage for the given units.
+     */
+    @NonNull
     public static DamageCalculator getCalculator(boolean weather, boolean horn, @NonNull Collection<UnitEntity> units) {
         DamageCalculatorBuilder builder = new DamageCalculatorBuilder();
         builder.setWeather(weather);
@@ -36,6 +51,17 @@ public class DamageCalculatorBuildDirector {
         return builder.getResult();
     }
 
+    /**
+     * Uses the given units
+     * to add a BondDamageCalculatorDecorator to the builder
+     * using DamageCalculatorBuilder#setBond().
+     * To accomplish this,
+     * the given unit list is converted to a Map from ids of units with the Ability.BINDING Ability to the respective squad size.
+     *
+     * @param units   Collection of UnitEntity objects that are in the row.
+     * @param builder DamageCalculatorBuilder where the horn buff is added.
+     * @see DamageCalculatorBuilder#setBond(Map)
+     */
     private static void setSquads(@NonNull Collection<UnitEntity> units, @NonNull DamageCalculatorBuilder builder) {
         Optional<Map<Integer, List<Integer>>> mapOptional = units.stream()
                 .filter(getAbilityPredicate(Ability.BINDING)) // filter for binding units only
@@ -63,16 +89,37 @@ public class DamageCalculatorBuildDirector {
         builder.setBond(idToSquadSize);
     }
 
+    /**
+     * Uses the given units
+     * to add a MoralDamageCalculatorDecorator to the builder using DamageCalculatorBuilder#setMoral().
+     * To accomplish this,
+     * the given unit list is converted to a List of the ids of the units with the Ability.MORAL_BOOST Ability.
+     *
+     * @param units   Collection of UnitEntity objects that are in the row.
+     * @param builder DamageCalculatorBuilder where the horn buff is added.
+     * @see DamageCalculatorBuilder#setMoral(List)
+     */
     private static void setMoralBoosts(@NonNull Collection<UnitEntity> units, @NonNull DamageCalculatorBuilder builder) {
-        if (units.stream().noneMatch(getAbilityPredicate(Ability.MORAL_BOOST))) {
-            return;
-        }
         List<Integer> unitIds = units.stream()
                 .filter(getAbilityPredicate(Ability.MORAL_BOOST))
-                .map(UnitEntity::getId).collect(Collectors.toList());
+                .map(UnitEntity::getId)
+                .collect(Collectors.toList());
         builder.setMoral(unitIds);
     }
 
+    /**
+     * Uses the given commander's horn status and the given units
+     * to add a HornDamageCalculatorDecorator to the builder
+     * using DamageCalculatorBuilder#setHorn().
+     * To accomplish this,
+     * the given unit list is converted to a List of the ids of the units with the Ability.HORN Ability
+     * (including ``null`` if horn is ``true``).
+     *
+     * @param horn    Boolean defining whether a commander's horn is in the row.
+     * @param units   Collection of UnitEntity objects that are in the row.
+     * @param builder DamageCalculatorBuilder where the horn buff is added.
+     * @see DamageCalculatorBuilder#setHorn(List)
+     */
     private static void setHorns(boolean horn, @NonNull Collection<UnitEntity> units, @NonNull DamageCalculatorBuilder builder) {
         List<Integer> unitIds = units.stream()
                 .filter(getAbilityPredicate(Ability.HORN))
@@ -84,6 +131,13 @@ public class DamageCalculatorBuildDirector {
         builder.setHorn(unitIds);
     }
 
+    /**
+     * Returns a Predicate checking if a UnitEntity has the given Ability.
+     *
+     * @param ability Ability for which units are checked.
+     * @return A Predicate checking whether a unit has the given Ability.
+     * @see Ability
+     */
     @NonNull
     @Contract(pure = true)
     private static Predicate<UnitEntity> getAbilityPredicate(Ability ability) {
