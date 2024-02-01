b1c20d565772de7b68229b39816de5a53dbd4672
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-02-01 18:54:35 +0100

    added RowStateUseCaseUnitTest.java
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

---
+++ app/src/test/java/com/peternaggschga/gwent/domain/RowStateUseCaseUnitTest.java
@@ -0,0 +1,51 @@
+package com.peternaggschga.gwent.domain;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.peternaggschga.gwent.data.Ability;
+import com.peternaggschga.gwent.data.RowType;
+import com.peternaggschga.gwent.data.UnitEntity;
+import com.peternaggschga.gwent.data.UnitRepository;
+import com.peternaggschga.gwent.ui.RowUiState;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Single;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RowStateUseCaseUnitTest {
+    private static final int TESTING_DAMAGE = 5;
+    private static final int TESTING_DEPTH = 50;
+
+    @Test
+    public void getRowStateReturnsCorrectState() {
+        UnitRepository repository = mock(UnitRepository.class);
+        UnitEntity entity = mock(UnitEntity.class);
+        when(entity.getAbility()).thenReturn(Ability.NONE);
+        when(entity.calculateDamage(any())).thenReturn(TESTING_DAMAGE);
+        for (RowType row : RowType.values()) {
+            for (boolean weather : new boolean[]{false, true}) {
+                for (boolean horn : new boolean[]{false, true}) {
+                    when(repository.isWeather(row)).thenReturn(Single.just(weather));
+                    when(repository.isHorn(row)).thenReturn(Single.just(horn));
+                    List<UnitEntity> entities = new ArrayList<>(TESTING_DEPTH);
+                    for (int i = 0; i < TESTING_DEPTH; i++) {
+                        when(repository.getUnits(row)).thenReturn(Single.just(entities));
+                        RowStateUseCase.getRowState(repository, row)
+                                .test()
+                                .assertValue(new RowUiState(i * TESTING_DAMAGE, weather, horn, i))
+                                .dispose();
+                        entities.add(entity);
+                    }
+                }
+            }
+        }
+    }
+}
