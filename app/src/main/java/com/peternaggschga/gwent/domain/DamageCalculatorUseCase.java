bc0109853eaf4ac8e334a6f81df17560e13db3b1
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-02-01 19:06:50 +0100

    added documentation for DamageCalculatorUseCase
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/gwent/domain/DamageCalculatorUseCase.java
+++ app/src/main/java/com/peternaggschga/gwent/domain/DamageCalculatorUseCase.java
@@ -12,7 +12,20 @@ import java.util.Collection;
 
 import io.reactivex.rxjava3.core.Single;
 
+/**
+ * A use case class responsible for creating DamageCalculator objects for rows using DamageCalculatorBuildDirector.
+ *
+ * @see DamageCalculatorBuildDirector
+ * @see DamageCalculator
+ */
 public class DamageCalculatorUseCase {
+    /**
+     * Returns a Single emitting a DamageCalculator object for the given row with properties retrieved from the given UnitRepository.
+     *
+     * @param repository UnitRepository used for data collection.
+     * @param row        RowType defining which row is queried for DamageCalculator generation.
+     * @return A Single emitting a DamageCalculator object for the given row.
+     */
     @NonNull
     public static Single<DamageCalculator> getDamageCalculator(@NonNull UnitRepository repository, @NonNull RowType row) {
         return Single.fromCallable(() -> {
