b18b23cbbfe01cb3403e0a7c211196f019287237
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-02-02 21:57:31 +0100

    added documentation to RowUiState
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/gwent/ui/RowUiState.java
+++ app/src/main/java/com/peternaggschga/gwent/ui/RowUiState.java
@@ -1,37 +1,85 @@
 package com.peternaggschga.gwent.ui;
 
+import static org.valid4j.Assertive.require;
+
 import androidx.annotation.IntRange;
 
+/**
+ * A data class encapsulating the visible state of a row in the main view of the application.
+ * Should only be created by com.peternaggschga.gwent.domain.cases.RowStateUseCase.
+ *
+ * @see com.peternaggschga.gwent.domain.cases.RowStateUseCase
+ */
 public class RowUiState {
+    /**
+     * Defines the summed-up damage of all units in this row.
+     */
     @IntRange(from = 0)
     private final int damage;
 
+    /**
+     * Defines whether the weather debuff is active in this row.
+     */
     private final boolean weather;
 
+    /**
+     * Defines whether the commander's horn buff is active in this row.
+     */
     private final boolean horn;
 
+    /**
+     * Defines the number of units in this row.
+     */
     @IntRange(from = 0)
     private final int units;
 
-    public RowUiState(int damage, boolean weather, boolean horn, int units) {
+    /**
+     * Constructor of a RowUiState encapsulating the given data.
+     * @param damage Integer representing the summed-up damage of all units.
+     * @param weather Boolean defining whether the weather debuff is active.
+     * @param horn Boolean defining whether the commander's horn buff is active.
+     * @param units Integer representing the number of units.
+     * @throws org.valid4j.errors.RequireViolation When damage or units is negative.
+     */
+    public RowUiState(@IntRange(from = 0) int damage, boolean weather, boolean horn, @IntRange(from = 0) int units) {
+        require(damage >= 0);
+        require(units >= 0);
         this.damage = damage;
         this.weather = weather;
         this.horn = horn;
         this.units = units;
     }
 
+    /**
+     * Returns the summed-up damage of all units in this row.
+     * @return An Integer representing the summed-up damage of all units.
+     */
+    @IntRange(from = 0)
     public int getDamage() {
         return damage;
     }
 
+    /**
+     * Returns whether the weather debuff is active in this row.
+     * @return A Boolean defining whether the weather debuff is active.
+     */
     public boolean isWeather() {
         return weather;
     }
 
+    /**
+     * Returns whether the commander's horn buff is active in this row.
+     * @return A Boolean whether the commander's horn buff is active.
+     */
     public boolean isHorn() {
         return horn;
     }
 
+    /**
+     * Returns the number of units in this row.
+     * @return An Integer representing the number of units in this row.
+     */
+    @IntRange(from = 0)
     public int getUnits() {
         return units;
     }
