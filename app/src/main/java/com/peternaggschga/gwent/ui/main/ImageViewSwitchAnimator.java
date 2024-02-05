3182729152534fc1beb22d53fd5c78722d21c5d3
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-02-05 19:12:26 +0100

    added ImageViewSwitchAnimator.java
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

---
+++ app/src/main/java/com/peternaggschga/gwent/ui/main/ImageViewSwitchAnimator.java
@@ -0,0 +1,52 @@
+package com.peternaggschga.gwent.ui.main;
+
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+
+import io.reactivex.rxjava3.core.Completable;
+
+public class ImageViewSwitchAnimator {
+    @NonNull
+    private static ImageView getOverlayView(@NonNull ImageView template) {
+        ImageView result = new ImageView(template.getContext());
+        result.setLayoutParams(new ViewGroup.LayoutParams(template.getLayoutParams()));
+        result.setImageDrawable(template.getDrawable());
+        result.setScaleType(template.getScaleType());
+        result.setTranslationZ(template.getTranslationZ() + 1);
+        return result;
+    }
+
+    @NonNull
+    public static Completable animatedSwitch(@NonNull ImageView view, @DrawableRes int resId) {
+        return Completable.create(emitter -> {
+            Animation fadeAnimation = AnimationUtils.loadAnimation(view.getContext(), android.R.anim.fade_out);
+            ImageView foregroundView = getOverlayView(view);
+            ViewGroup parent = (ViewGroup) view.getParent();
+
+            fadeAnimation.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    view.setImageResource(resId);
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    parent.removeView(foregroundView);
+                    emitter.onComplete();
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+                }
+            });
+
+            parent.addView(foregroundView);
+            foregroundView.startAnimation(fadeAnimation);
+        });
+    }
+}
