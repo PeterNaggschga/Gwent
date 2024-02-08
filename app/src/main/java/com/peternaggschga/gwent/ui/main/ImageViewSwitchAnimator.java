4d0bcfba06206b2e7507622ac93ad55501790fc6
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-02-08 16:24:03 +0100

    added documentation to ImageViewSwitchAnimator.java
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/gwent/ui/main/ImageViewSwitchAnimator.java
+++ app/src/main/java/com/peternaggschga/gwent/ui/main/ImageViewSwitchAnimator.java
@@ -10,7 +10,19 @@ import androidx.annotation.NonNull;
 
 import io.reactivex.rxjava3.core.Completable;
 
+/**
+ * A class providing functionality for changing
+ * the resource shown by an ImageView using a fading animation.
+ *
+ * @todo Remove res/anim
+ * @see #animatedSwitch(ImageView, int)
+ */
 public class ImageViewSwitchAnimator {
+    /**
+     * Returns a copy of the given template which is one layer above it.
+     * @param template ImageView that is copied.
+     * @return An ImageView similar to template but one layer in front of it.
+     */
     @NonNull
     private static ImageView getOverlayView(@NonNull ImageView template) {
         ImageView result = new ImageView(template.getContext());
@@ -23,6 +35,16 @@ public class ImageViewSwitchAnimator {
         return result;
     }
 
+    /**
+     * Changes the resource of the given ImageView in an animation to the given resource id.
+     * Animation is realized by creating a new ImageView on top of the given ImageView
+     * which is then faded out using the ``fade_out`` animation provided by Android.
+     * The operation is complete when the animation ends.
+     * @see android.R.anim#fade_out
+     * @param view ImageView, whose resource is to be changed.
+     * @param resId Integer representing the drawable resource that the given view should show.
+     * @return A Completable tracking operation status.
+     */
     @NonNull
     public static Completable animatedSwitch(@NonNull ImageView view, @DrawableRes int resId) {
         return Completable.create(emitter -> {
