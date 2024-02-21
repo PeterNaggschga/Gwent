20ca53273910b7cf86579690115494cb67a2f19d
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-02-21 21:50:04 +0100

    added documentation to CoinFlipDialog
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/gwent/ui/dialogs/CoinFlipDialog.java
+++ app/src/main/java/com/peternaggschga/gwent/ui/dialogs/CoinFlipDialog.java
@@ -17,13 +17,40 @@ import com.peternaggschga.gwent.R;
 import java.util.Objects;
 import java.util.Random;
 
+/**
+ * A Dialog class showing the result of a coin-flip.
+ * Shows heads or tails with 48.5% probability respectively.
+ * The remaining 3% are divided equally between three tie situations.
+ *
+ * @see R.layout#popup_coin_normal
+ * @see R.layout#popup_coin_stewie
+ * @see R.layout#popup_coin_terry
+ * @see R.layout#popup_coin_vin
+ */
 public class CoinFlipDialog extends Dialog {
+    /**
+     * Random used to decide the result of the coin-flip.
+     */
     private static final Random random = new Random();
 
+    /**
+     * Constructor of a CoinFlipDialog in the given Context.
+     * @param context Context in which the Dialog is run.
+     */
     public CoinFlipDialog(@NonNull Context context) {
         super(context);
     }
 
+    /**
+     * Initializes Layout using #setContentView(int) and sets listeners for each view.
+     * The result of the coin-flip is decided here.
+     *
+     * @param savedInstanceState If this dialog is being reinitialized after
+     *                           the hosting activity was previously shut down, holds the result from
+     *                           the most recent call to {@link #onSaveInstanceState}, or null if this
+     *                           is the first time.
+     * @see #setContentView(int)
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
