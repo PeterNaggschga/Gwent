e29d5dbc5ac52badad108585c093afc56a6167c9
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-02-23 19:07:13 +0100

    added documentation to ResetRepositoryUseCase
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/gwent/domain/cases/ResetRepositoryUseCase.java
+++ app/src/main/java/com/peternaggschga/gwent/domain/cases/ResetRepositoryUseCase.java
@@ -2,6 +2,7 @@ package com.peternaggschga.gwent.domain.cases;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 
 import androidx.annotation.IntRange;
 import androidx.annotation.NonNull;
@@ -15,10 +16,22 @@ import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.CompletableEmitter;
 
 /**
- * A use case class responsible for removing units or resetting the UnitRepository.
- * Also, capable of invoking an AlertDialog if a UnitEntity with the Ability#REVENGE ability is removed.
+ * A use case class responsible for resetting the UnitRepository.
+ * Capable of invoking a Dialog if a UnitEntity with the Ability#REVENGE ability is removed.
  */
 public class ResetRepositoryUseCase {
+    /**
+     * Resets the given UnitRepository and keeps the given UnitEntity (if not null).
+     * If a removed UnitEntity has the Ability#REVENGE ability,
+     * a Dialog asking whether the ability should be used is shown.
+     *
+     * @param context    Context of the shown Dialog.
+     * @param repository UnitRepository that is being reset.
+     * @param keptUnit   UnitEntity that should be kept.
+     * @return A Completable tracking operation status.
+     * @see #getRevengeDialog(Context, UnitRepository, CompletableEmitter, UnitEntity, long)
+     * @see UnitRepository#reset(UnitEntity)
+     */
     @NonNull
     public static Completable reset(@NonNull Context context, @NonNull UnitRepository repository, @Nullable UnitEntity keptUnit) {
         return repository.getUnits()
@@ -35,6 +48,20 @@ public class ResetRepositoryUseCase {
                 });
     }
 
+    /**
+     * Creates a Dialog asking whether the Ability#REVENGE ability should be activated.
+     * The Dialog is created using RevengeDialogFactory.
+     *
+     * @param context      Context of the shown Dialog.
+     * @param repository   UnitRepository where the UnitEntity objects are removed and avengers are inserted.
+     * @param emitter      CompletableEmitter where CompletableEmitter#onComplete must be called,
+     *                     when the user makes a decision.
+     * @param keptUnit     UnitEntity that should be kept.
+     * @param revengeUnits Long representing the number of revenge units.
+     * @return A Dialog asking whether the Ability#REVENGE ability should be activated.
+     * @see RevengeDialogFactory#getRevengeDialog(Context, DialogInterface.OnClickListener, DialogInterface.OnClickListener)
+     * @see RevengeDialogFactory#insertAvengers(UnitRepository, long)
+     */
     @NonNull
     private static Dialog getRevengeDialog(@NonNull Context context, @NonNull UnitRepository repository,
                                            @NonNull CompletableEmitter emitter, @Nullable UnitEntity keptUnit,
@@ -53,6 +80,17 @@ public class ResetRepositoryUseCase {
         );
     }
 
+    /**
+     * Resets the given UnitRepository.
+     * If a removed UnitEntity has the Ability#REVENGE ability,
+     * a Dialog asking whether the ability should be used is shown.
+     * Wrapper of #reset(Context, UnitRepository, UnitEntity).
+     *
+     * @param context    Context of the shown Dialog.
+     * @param repository UnitRepository that is being reset.
+     * @return A Completable tracking operation status.
+     * @see #reset(Context, UnitRepository, UnitEntity)
+     */
     @NonNull
     public static Completable reset(@NonNull Context context, @NonNull UnitRepository repository) {
         return reset(context, repository, null);
