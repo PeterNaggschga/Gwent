1072d1f2f5a05107778af660b02e403ab0ae216c
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-03-28 23:45:28 +0100

    added ShowUnitsDialog.java (empty)
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

---
+++ app/src/main/java/com/peternaggschga/gwent/ui/dialogs/ShowUnitsDialog.java
@@ -0,0 +1,12 @@
+package com.peternaggschga.gwent.ui.dialogs;
+
+import android.app.Dialog;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+public class ShowUnitsDialog extends Dialog {
+    public ShowUnitsDialog(@NonNull Context context) {
+        super(context);
+    }
+}
