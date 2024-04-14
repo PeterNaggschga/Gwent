c261079d47e8348d00f29b753521b9e9ec14d0bd
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-04-14 23:06:38 +0200

    added documentation to CardUiStateFactory
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/gwent/ui/dialogs/cards/CardUiStateFactory.java
+++ app/src/main/java/com/peternaggschga/gwent/ui/dialogs/cards/CardUiStateFactory.java
@@ -14,22 +14,47 @@ import com.peternaggschga.gwent.data.UnitEntity;
 import com.peternaggschga.gwent.domain.cases.DamageCalculatorUseCase;
 import com.peternaggschga.gwent.domain.damage.DamageCalculator;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
- * @todo Documentation
+ * A factory class responsible for creating CardUiState objects from UnitEntity objects.
  * @todo Add testing.
  */
 public class CardUiStateFactory {
+    /**
+     * Boolean defining the status of the weather debuff in the row
+     * for which CardUiState objects are created.
+     */
     private final boolean weather;
+
+    /**
+     * Boolean defining the status of the horn buff in the row
+     * for which CardUiState objects are created.
+     */
     private final boolean horn;
+
+    /**
+     * Map containing a color integer for each Color value possible,
+     * i.e., Color#DEFAULT, Color#BUFFED, and Color#DEBUFFED.
+     */
     @NonNull
     private final Map<Color, Integer> damageTextColors = new HashMap<>(Color.values().length);
 
+    /**
+     * Constructor of a CardUiStateFactory for a row with the given weather and horn
+     * (de-)buff values.
+     * The Context parameter is used to retrieve the color values saved in #damageTextColors.
+     *
+     * @param context Context object used to get colors.
+     * @param weather Boolean defining whether the weather debuff is active.
+     * @param horn    Boolean defining whether the horn buff is active.
+     * @see Context#getColor(int)
+     */
     public CardUiStateFactory(@NonNull Context context, boolean weather, boolean horn) {
         this.weather = weather;
         this.horn = horn;
@@ -38,16 +63,28 @@ public class CardUiStateFactory {
         damageTextColors.put(DEBUFFED, context.getColor(R.color.color_damage_textColor_debuffed));
     }
 
+    /**
+     * Creates a List of CardUiState objects from the given List of UnitEntity objects.
+     * Basically calls #createCardUiState(UnitEntity, DamageCalculator) for each given UnitEntity.
+     * @see #createCardUiState(UnitEntity, DamageCalculator)
+     * @param units List of UnitEntity objects that are converted to CardUiState objects.
+     * @return A List of CardUiState objects from the given UnitEntity objects.
+     */
     @NonNull
-    public List<CardUiState> createCardUiState(@NonNull List<UnitEntity> units) {
+    public List<CardUiState> createCardUiState(@NonNull Collection<UnitEntity> units) {
         DamageCalculator calculator = DamageCalculatorUseCase.getDamageCalculator(weather, horn, units);
-        List<CardUiState> result = new ArrayList<>(units.size());
-        for (UnitEntity unit : units) {
-            result.add(createCardUiState(unit, calculator));
-        }
-        return result;
+        return units.stream()
+                .map(unit -> createCardUiState(unit, calculator))
+                .collect(Collectors.toList());
     }
 
+    /**
+     * Creates a CardUiState from the given UnitEntity.
+     * @see #createCardUiState(Collection)
+     * @param unit UnitEntity that is converted to a CardUiState.
+     * @param calculator DamageCalculator used to calculate damage and Color of the given UnitEntity.
+     * @return A CardUiState obtained from the given UnitEntity.
+     */
     @NonNull
     public CardUiState createCardUiState(@NonNull UnitEntity unit, @NonNull DamageCalculator calculator) {
         int damageBackgroundId = R.drawable.icon_damage_background;
