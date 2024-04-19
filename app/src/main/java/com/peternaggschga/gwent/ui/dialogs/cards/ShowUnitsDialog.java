4cc0e58601d587567cf4cac7face041120078cfb
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-04-19 18:11:47 +0200

    added documentation to ShowUnitsDialog
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/gwent/ui/dialogs/cards/ShowUnitsDialog.java
+++ app/src/main/java/com/peternaggschga/gwent/ui/dialogs/cards/ShowUnitsDialog.java
@@ -17,14 +17,35 @@ import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.disposables.Disposable;
 
 /**
- * @todo Documentation
+ * An OverlayDialog used to list the units of a certain row and enabling the user to copy, add,
+ * or delete new units.
+ * @see CardListAdapter
  */
 public class ShowUnitsDialog extends OverlayDialog {
+    /**
+     * CardListAdapter offering a list of CardUiState objects to the UI.
+     */
     @NonNull
     private final CardListAdapter cardListAdapter;
+
+    /**
+     * Disposable associated with a Flowable regularly updating the unit list in #cardListAdapter.
+     * Is being disposed in an android.content.DialogInterface.OnDismissListener that is set in #onCreate().
+     *
+     * @see android.content.DialogInterface.OnDismissListener
+     * @see #onCreate(Bundle)
+     */
     @NonNull
     private final Disposable updateSubscription;
 
+    /**
+     * Constructor of a ShowUnitsDialog shown in the given Context,
+     * with the given CardListAdapter and update subscription.
+     * @param context Context this Dialog is shown in.
+     * @param cardListAdapter CardListAdapter providing an always up-to-date list of CardUiState objects for a certain row.
+     * @param updateSubscription Disposable managing the running updates of the given CardListAdapter.
+     *                           When disposed, the adapter is not updated anymore.
+     */
     private ShowUnitsDialog(@NonNull Context context, @NonNull CardListAdapter cardListAdapter,
                             @NonNull Disposable updateSubscription) {
         super(context, R.layout.popup_cards);
@@ -32,6 +53,12 @@ public class ShowUnitsDialog extends OverlayDialog {
         this.updateSubscription = updateSubscription;
     }
 
+    /**
+     * Creates a new ShowUnitsDialog in the given Context and for the given row.
+     * @param context Context the Dialog is shown in.
+     * @param row RowType defining the row that is represented by this Dialog.
+     * @return A Single emitting the created ShowUnitsDialog.
+     */
     @NonNull
     public static Single<ShowUnitsDialog> getDialog(@NonNull Context context, @NonNull RowType row) {
         return GwentApplication.getRepository(context)
@@ -51,6 +78,17 @@ public class ShowUnitsDialog extends OverlayDialog {
                 );
     }
 
+    /**
+     * Initializes the RecyclerView by connecting it to the #cardListAdapter
+     * and sets View.OnClickListener for each button.
+     * Also registers a RecyclerView.AdapterDataObserver responsible for scrolling to the end of the RecyclerView
+     * whenever an item is being inserted.
+     * @see CardListAdapter#registerAdapterDataObserver(RecyclerView.AdapterDataObserver)
+     * @param savedInstanceState If this dialog is being reinitialized after
+     *                           the hosting activity was previously shut down, holds the result from
+     *                           the most recent call to {@link #onSaveInstanceState}, or null if this
+     *                           is the first time.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,10 +98,13 @@ public class ShowUnitsDialog extends OverlayDialog {
         RecyclerView recyclerView = findViewById(R.id.cardsList);
         recyclerView.setLayoutManager(llm);
         recyclerView.setAdapter(cardListAdapter);
+
         cardListAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
-            /**
-             * @param positionStart
-             * @param itemCount
+            /** Smoothly scrolls to the last item in the RecyclerView
+             * to show the user that an item has been inserted.
+             * @see RecyclerView#smoothScrollToPosition(int)
+             * @param positionStart Integer defining the first position from where new items are inserted.
+             * @param itemCount Integer defining how many items have been inserted.
              */
             @Override
             public void onItemRangeInserted(int positionStart, int itemCount) {
@@ -74,7 +115,7 @@ public class ShowUnitsDialog extends OverlayDialog {
 
         findViewById(R.id.popup_cards_add_button).setOnClickListener(v -> {
             dismiss();
-            // TODO
+            // TODO: hide()
         });
 
         findViewById(R.id.popup_cards_cancel_button).setOnClickListener(v -> dismiss());
