25ffdcd5c0e4e5e24f63651ea3feb2e68df2ab30
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-05-28 22:29:41 +0200

    added documentation to GwentApplication
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/gwent/GwentApplication.java
+++ app/src/main/java/com/peternaggschga/gwent/GwentApplication.java
@@ -12,26 +12,57 @@ import com.peternaggschga.gwent.data.UnitRepository;
 import io.reactivex.rxjava3.core.Single;
 
 /**
- * @todo Documentation
+ * An {@link Application} encapsulating the Gwent app.
+ * This class is responsible for maintaining a Singleton {@link UnitRepository} connected to the {@link AppDatabase} of this app.
  */
 public class GwentApplication extends Application {
-    private AppDatabase database;
+    /**
+     * {@link UnitRepository} used for communication to {@link #database}.
+     * Is lazily initialized in {@link #getRepository()} and provided as a Singleton.
+     * @see #getRepository()
+     * @see #getRepository(Context)
+     */
     private static UnitRepository repository = null;
+    /**
+     * {@link AppDatabase} used by this app.
+     * Is initialized in {@link #onCreate()}.
+     *
+     * @see #onCreate()
+     */
+    private AppDatabase database;
 
+    /**
+     * Returns a {@link UnitRepository} used by the {@link GwentApplication} referenced by the given {@link Context}.
+     * Wrapper of {@link #getRepository()}.
+     * @see #getRepository()
+     * @param context {@link Context} of a {@link GwentApplication}.
+     * @return A {@link UnitRepository} used by the given {@link GwentApplication} to access game state.
+     */
+    public static Single<UnitRepository> getRepository(@NonNull Context context) {
+        return ((GwentApplication) context.getApplicationContext()).getRepository();
+    }
+
+    /**
+     * Called when the application is starting, before any activity, service,
+     * or receiver objects (excluding content providers) have been created.
+     * Initializes {@link #database}.
+     */
     @Override
     public void onCreate() {
         super.onCreate();
-        database = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database").build();
+        database = Room.databaseBuilder(this, AppDatabase.class, "database").build();
     }
 
+    /**
+     * Returns the {@link #repository} used to create, read, update and delete game state.
+     * Lazily initializes {@link #repository}, if not yet done so.
+     * @see #getRepository(Context)
+     * @return A {@link UnitRepository} used to access game state.
+     */
     public Single<UnitRepository> getRepository() {
         if (repository != null) {
             return Single.just(repository);
         }
         return UnitRepository.getRepository(database).doOnSuccess(unitRepository -> repository = unitRepository);
-    }
-
-    public static Single<UnitRepository> getRepository(@NonNull Context context) {
-        return ((GwentApplication) context.getApplicationContext()).getRepository();
     }
 }
