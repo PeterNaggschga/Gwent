59a3ad311636d71eeb5079b0cab4fe2103b9afd1
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-05-31 21:07:18 +0200

    added documentation to SectionsPagerAdapter
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/gwent/ui/introduction/SectionsPagerAdapter.java
+++ app/src/main/java/com/peternaggschga/gwent/ui/introduction/SectionsPagerAdapter.java
@@ -6,20 +6,36 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 /**
- * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * A {@link FragmentStateAdapter} that returns an {@link PlaceholderFragment} corresponding to
  * one of the sections/tabs/pages.
- * @todo Documentation
+ * @todo Make package-private.
  */
 public class SectionsPagerAdapter extends FragmentStateAdapter {
+
+    /**
+     * Constructor of a SectionsPagerAdapter for the given {@link FragmentActivity}.
+     *
+     * @param fragmentActivity {@link FragmentActivity} that uses this adapter.
+     */
     public SectionsPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
         super(fragmentActivity);
     }
 
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     * Essentially queries {@link PlaceholderFragment#PAGES_COUNT}.
+     * @return An {@link Integer} defining the number of pages.
+     */
     @Override
     public int getItemCount() {
         return PlaceholderFragment.PAGES_COUNT;
     }
 
+    /**
+     * Provides a new {@link PlaceholderFragment} associated with the specified position.
+     * @param position {@link Integer} defining the page that is queried.
+     * @return A {@link Fragment} that will be shown at the specified position.
+     */
     @NonNull
     @Override
     public Fragment createFragment(int position) {
