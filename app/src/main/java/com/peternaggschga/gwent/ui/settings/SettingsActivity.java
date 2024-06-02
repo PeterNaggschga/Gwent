2e02207a10a385743af532e9d4072d3f9526a9be
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-06-02 23:17:58 +0200

    moved Settings fragments to own class files
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/gwent/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/peternaggschga/gwent/ui/settings/SettingsActivity.java
@@ -1,14 +1,5 @@
 package com.peternaggschga.gwent.ui.settings;
 
-import static com.peternaggschga.gwent.ui.settings.RuleSection.CARDS;
-import static com.peternaggschga.gwent.ui.settings.RuleSection.CARD_ABILITIES;
-import static com.peternaggschga.gwent.ui.settings.RuleSection.COMMANDER;
-import static com.peternaggschga.gwent.ui.settings.RuleSection.COURSE;
-import static com.peternaggschga.gwent.ui.settings.RuleSection.FACTIONS;
-import static com.peternaggschga.gwent.ui.settings.RuleSection.GENERAL;
-import static com.peternaggschga.gwent.ui.settings.RuleSection.SPECIAL_CARDS;
-
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -22,12 +13,8 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.peternaggschga.gwent.R;
-import com.peternaggschga.gwent.ui.introduction.IntroductionActivity;
 import com.peternaggschga.gwent.ui.main.FactionSwitchListener;
 
-import org.jetbrains.annotations.Contract;
-
-import java.io.Serializable;
 import java.util.Objects;
 
 /**
@@ -36,7 +23,7 @@ import java.util.Objects;
  */
 public class SettingsActivity extends AppCompatActivity implements PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
     /**
-     * Initializes layout and {@link ActionBar} as well as creates and displays a new {@link HeaderFragment}.
+     * Initializes layout and {@link ActionBar} as well as creates and displays a new {@link SettingsHeaderFragment}.
      *
      * @param savedInstanceState If the activity is being re-initialized after
      *                           previously being shut down then this Bundle contains the data it most
@@ -56,7 +43,7 @@ public class SettingsActivity extends AppCompatActivity implements PreferenceFra
         }
 
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.settingsFrameLayout, new HeaderFragment())
+                .replace(R.id.settingsFrameLayout, new SettingsHeaderFragment())
                 .commit();
     }
 
@@ -116,117 +103,5 @@ public class SettingsActivity extends AppCompatActivity implements PreferenceFra
                 .commit();
         setTitle(pref.getTitle());
         return true;
-    }
-
-    /**
-     * A {@link PreferenceFragmentCompat} class encapsulating the main preference screen,
-     * i.e., the {@link Preference}s defined in {@link R.xml#header_preferences}.
-     * @todo Move to own class file.
-     */
-    public static class HeaderFragment extends PreferenceFragmentCompat {
-        /**
-         * Called during {@link #onCreate(Bundle)} to supply the preferences for this fragment.
-         * Sets shown {@link Preference}s from {@link R.xml#header_preferences}
-         * and registers an {@link androidx.preference.Preference.OnPreferenceClickListener}
-         * on the {@link Preference} at {@link R.string#preference_key_introduction} to start a new {@link IntroductionActivity}.
-         * @param savedInstanceState If the fragment is being re-created from a previous saved state,
-         *                           this is the state.
-         * @param rootKey            If non-null, this preference fragment should be rooted at the
-         *                           {@link androidx.preference.PreferenceScreen} with this key.
-         */
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.header_preferences, rootKey);
-
-            Preference introductionPreference = Objects.requireNonNull(findPreference(getString(R.string.preference_key_introduction)));
-            introductionPreference.setOnPreferenceClickListener(preference -> {
-                startActivity(new Intent(getContext(), IntroductionActivity.class));
-                return true;
-            });
-        }
-    }
-
-    /**
-     * A {@link PreferenceFragmentCompat} class encapsulating the sound preference screen,
-     * i.e., the {@link Preference}s defined in {@link R.xml#sound_preferences}.
-     * @todo Move to own class file.
-     */
-    public static class SoundFragment extends PreferenceFragmentCompat {
-        /**
-         * Called during {@link #onCreate(Bundle)} to supply the preferences for this fragment.
-         * Sets shown {@link Preference}s from {@link R.xml#sound_preferences}.
-         * @param savedInstanceState If the fragment is being re-created from a previous saved state,
-         *                           this is the state.
-         * @param rootKey            If non-null, this preference fragment should be rooted at the
-         *                           {@link androidx.preference.PreferenceScreen} with this key.
-         */
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.sound_preferences, rootKey);
-        }
-    }
-
-    /**
-     * A {@link PreferenceFragmentCompat} class encapsulating the rule preference screen,
-     * i.e., the rule sections defined in {@link R.xml#rule_preferences}.
-     * @todo Move to own class file.
-     */
-    public static class RuleHeaderFragment extends PreferenceFragmentCompat {
-        /**
-         * Creates an {@link Preference.OnPreferenceClickListener} that starts a new {@link RuleActivity}
-         * for the given {@link RuleSection} using an {@link Intent}.
-         * The {@link Intent} provides the requested {@link RuleSection} to the {@link RuleActivity}
-         * using {@link Intent#putExtra(String, Serializable)} with {@link RuleActivity#INTENT_EXTRA_TAG} as a tag.
-         * @param section {@link RuleSection} that is requested.
-         * @return An {@link Preference.OnPreferenceClickListener} calling a {@link RuleActivity}.
-         */
-        @NonNull
-        @Contract(pure = true)
-        private Preference.OnPreferenceClickListener getSectionClickListener(@NonNull RuleSection section) {
-            return preference -> {
-                startActivity(
-                        new Intent(getContext(), RuleActivity.class)
-                                .putExtra(RuleActivity.INTENT_EXTRA_TAG, section)
-                );
-                return true;
-            };
-        }
-
-        /**
-         * Called during {@link #onCreate(Bundle)} to supply the preferences for this fragment.
-         * Sets shown {@link Preference}s from {@link R.xml#rule_preferences}.
-         * Also provides each element with an {@link Preference.OnPreferenceClickListener}
-         * that starts a new {@link RuleActivity} for the respective {@link RuleSection}.
-         * @see #getSectionClickListener(RuleSection)
-         * @param savedInstanceState If the fragment is being re-created from a previous saved state,
-         *                           this is the state.
-         * @param rootKey            If non-null, this preference fragment should be rooted at the
-         *                           {@link androidx.preference.PreferenceScreen} with this key.
-         */
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.rule_preferences, rootKey);
-
-            Preference rulesGeneral = Objects.requireNonNull(findPreference(getString(R.string.preference_rules_general_key)));
-            rulesGeneral.setOnPreferenceClickListener(getSectionClickListener(GENERAL));
-
-            Preference rulesCourse = Objects.requireNonNull(findPreference(getString(R.string.preference_rules_course_key)));
-            rulesCourse.setOnPreferenceClickListener(getSectionClickListener(COURSE));
-
-            Preference ruleFactions = Objects.requireNonNull(findPreference(getString(R.string.preference_rules_factions_key)));
-            ruleFactions.setOnPreferenceClickListener(getSectionClickListener(FACTIONS));
-
-            Preference rulesCommander = Objects.requireNonNull(findPreference(getString(R.string.preference_rules_commander_key)));
-            rulesCommander.setOnPreferenceClickListener(getSectionClickListener(COMMANDER));
-
-            Preference rulesCards = Objects.requireNonNull(findPreference(getString(R.string.preference_rules_cards_key)));
-            rulesCards.setOnPreferenceClickListener(getSectionClickListener(CARDS));
-
-            Preference ruleCardAbilities = Objects.requireNonNull(findPreference(getString(R.string.preference_rules_card_abilities_key)));
-            ruleCardAbilities.setOnPreferenceClickListener(getSectionClickListener(CARD_ABILITIES));
-
-            Preference rulesSpecialCards = Objects.requireNonNull(findPreference(getString(R.string.preference_rules_special_cards_key)));
-            rulesSpecialCards.setOnPreferenceClickListener(getSectionClickListener(SPECIAL_CARDS));
-        }
     }
 }
---
+++ app/src/main/java/com/peternaggschga/gwent/ui/settings/SettingsRuleFragment.java
@@ -0,0 +1,88 @@
+package com.peternaggschga.gwent.ui.settings;
+
+import static com.peternaggschga.gwent.ui.settings.RuleSection.CARDS;
+import static com.peternaggschga.gwent.ui.settings.RuleSection.CARD_ABILITIES;
+import static com.peternaggschga.gwent.ui.settings.RuleSection.COMMANDER;
+import static com.peternaggschga.gwent.ui.settings.RuleSection.COURSE;
+import static com.peternaggschga.gwent.ui.settings.RuleSection.FACTIONS;
+import static com.peternaggschga.gwent.ui.settings.RuleSection.GENERAL;
+import static com.peternaggschga.gwent.ui.settings.RuleSection.SPECIAL_CARDS;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.peternaggschga.gwent.R;
+
+import org.jetbrains.annotations.Contract;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A {@link PreferenceFragmentCompat} class encapsulating the rule preference screen,
+ * i.e., the rule sections defined in {@link R.xml#rule_preferences}.
+ */
+public class SettingsRuleFragment extends PreferenceFragmentCompat {
+    /**
+     * Creates an {@link Preference.OnPreferenceClickListener} that starts a new {@link RuleActivity}
+     * for the given {@link RuleSection} using an {@link Intent}.
+     * The {@link Intent} provides the requested {@link RuleSection} to the {@link RuleActivity}
+     * using {@link Intent#putExtra(String, Serializable)} with {@link RuleActivity#INTENT_EXTRA_TAG} as a tag.
+     *
+     * @param section {@link RuleSection} that is requested.
+     * @return An {@link Preference.OnPreferenceClickListener} calling a {@link RuleActivity}.
+     */
+    @NonNull
+    @Contract(pure = true)
+    private Preference.OnPreferenceClickListener getSectionClickListener(@NonNull RuleSection section) {
+        return preference -> {
+            startActivity(
+                    new Intent(getContext(), RuleActivity.class)
+                            .putExtra(RuleActivity.INTENT_EXTRA_TAG, section)
+            );
+            return true;
+        };
+    }
+
+    /**
+     * Called during {@link #onCreate(Bundle)} to supply the preferences for this fragment.
+     * Sets shown {@link Preference}s from {@link R.xml#rule_preferences}.
+     * Also provides each element with an {@link Preference.OnPreferenceClickListener}
+     * that starts a new {@link RuleActivity} for the respective {@link RuleSection}.
+     *
+     * @param savedInstanceState If the fragment is being re-created from a previous saved state,
+     *                           this is the state.
+     * @param rootKey            If non-null, this preference fragment should be rooted at the
+     *                           {@link androidx.preference.PreferenceScreen} with this key.
+     * @see #getSectionClickListener(RuleSection)
+     */
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.rule_preferences, rootKey);
+
+        Preference rulesGeneral = Objects.requireNonNull(findPreference(getString(R.string.preference_rules_general_key)));
+        rulesGeneral.setOnPreferenceClickListener(getSectionClickListener(GENERAL));
+
+        Preference rulesCourse = Objects.requireNonNull(findPreference(getString(R.string.preference_rules_course_key)));
+        rulesCourse.setOnPreferenceClickListener(getSectionClickListener(COURSE));
+
+        Preference ruleFactions = Objects.requireNonNull(findPreference(getString(R.string.preference_rules_factions_key)));
+        ruleFactions.setOnPreferenceClickListener(getSectionClickListener(FACTIONS));
+
+        Preference rulesCommander = Objects.requireNonNull(findPreference(getString(R.string.preference_rules_commander_key)));
+        rulesCommander.setOnPreferenceClickListener(getSectionClickListener(COMMANDER));
+
+        Preference rulesCards = Objects.requireNonNull(findPreference(getString(R.string.preference_rules_cards_key)));
+        rulesCards.setOnPreferenceClickListener(getSectionClickListener(CARDS));
+
+        Preference ruleCardAbilities = Objects.requireNonNull(findPreference(getString(R.string.preference_rules_card_abilities_key)));
+        ruleCardAbilities.setOnPreferenceClickListener(getSectionClickListener(CARD_ABILITIES));
+
+        Preference rulesSpecialCards = Objects.requireNonNull(findPreference(getString(R.string.preference_rules_special_cards_key)));
+        rulesSpecialCards.setOnPreferenceClickListener(getSectionClickListener(SPECIAL_CARDS));
+    }
+}
